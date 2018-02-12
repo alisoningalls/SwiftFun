00dbcc9a8c08a85318b3de5cefef75110022397e
Alison Ingalls <alisoningalls@alisons-macbook-air.local>
2018-02-12 08:29:47 -0500

    Initial Commit

---
+++ Swift Fun/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Swift Fun
+//
+//  Created by Alison Ingalls on 2/7/18.
+//  Copyright © 2018 Alison Ingalls. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    var buttonCount = 0
+    
+    @IBOutlet weak var MyLabel: UILabel!
+    
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        buttonCount = buttonCount + 1
+        
+        print(buttonCount)
+    
+        if buttonCount >= 10{
+        
+            view.backgroundColor = UIColor.red
+   
+            MyLabel.text = "You hit it more than 10 times!"
+            
+        }
+
+    }
+}
+
